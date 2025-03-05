ada1fe1485d140290a5ac5693b4803f10d0100e7
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2025-03-05 12:28:38 +0530

    fix: valid error showing even if response fails

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -53,9 +53,6 @@ function Home() {
             body: JSON.stringify(requestBody),
         })
             .then((response) => {
-                if (!response.ok) {
-                    throw new Error(`HTTP error! Status: ${response.status}`);
-                }
                 return response.json();
             })
             .then((data) => {
